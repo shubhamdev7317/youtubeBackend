0453666157f17af6df4b0e947410f54b0f1e6d09
Subhash Sagar Singh <shubhamdev7317@example.com>
2024-12-17 01:24:53 +0530

    Utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) =>{
+   (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err) => next(err))
+   }
+}
+
+export {asyncHandler}
+
+// const asyncHandler = (fn) => async (req, resizeBy, next) => {
+//     try{
+//         await fn(req, resizeBy, next)
+//     }catch (error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
